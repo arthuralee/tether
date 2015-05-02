b76b3f88a5cffb82a0abfee2131645c88baf61ee
akshalaby <akshalaby@gmail.com>
2015-05-02 03:04:23 -0400

    Adafruit_9dof library updated getFusionSensor

--- arduino/arduino/libraries/Adafruit_9DOF/Adafruit_9DOF.cpp
+++ arduino/arduino/libraries/Adafruit_9DOF/Adafruit_9DOF.cpp
@@ -1,19 +1,19 @@
 /***************************************************************************
   This is a library for the Adafruit 9DOF Breakout
-
   Designed specifically to work with the Adafruit 9DOF Breakout:
   http://www.adafruit.com/products/1714
-
   These displays use I2C to communicate, 2 pins are required to interface.
-
   Adafruit invests time and resources providing this open source code,
   please support Adafruit andopen-source hardware by purchasing products
   from Adafruit!
-
-  Written by Kevin Townsend for Adafruit Industries.
+  Written by Kevin Townsend for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
-#include "Arduino.h"
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
 
 #include <Wire.h>
 #include <limits.h>
@@ -31,20 +31,20 @@
 /***************************************************************************
  CONSTRUCTOR
  ***************************************************************************/
-
+ 
 /**************************************************************************/
 /*!
     @brief  Instantiates a new Adafruit_9DOF class
 */
 /**************************************************************************/
-Adafruit_9DOF::Adafruit_9DOF(void)
+Adafruit_9DOF::Adafruit_9DOF(void) 
 {
 }
 
 /***************************************************************************
  PUBLIC FUNCTIONS
  ***************************************************************************/
-
+ 
 /**************************************************************************/
 /*!
     @brief  Setups the HW
@@ -62,23 +62,20 @@ bool Adafruit_9DOF::begin()
 /*!
     @brief  Populates the .pitch/.roll fields in the sensors_vec_t struct
             with the right angular data (in degree)
-
     @param  event         The sensors_event_t variable containing the
                           data from the accelerometer
     @param  orientation   The sensors_vec_t object that will have it's
                           .pitch and .roll fields populated
     @return Returns true if the operation was successful, false if there
             was an error
-
+            
     @code
-
     bool error;
     sensors_event_t event;
     sensors_vec_t orientation;
     ...
     lsm303accelGetSensorEvent(&event);
     error = accelGetOrientation(&event, &orientation);
-
     @endcode
 */
 /**************************************************************************/
@@ -102,10 +99,7 @@ bool Adafruit_9DOF::accelGetOrientation(sensors_event_t *event, sensors_vec_t *o
 
   t_roll = event->acceleration.x * event->acceleration.x + event->acceleration.z * event->acceleration.z;
   orientation->roll = (float)atan2(event->acceleration.y, sqrt(t_roll)) * 180 / PI;
-  if(event->acceleration.z < 0){
-    if(orientation->roll > 0) orientation->roll = 180 - orientation->roll;
-    if(orientation->roll < 0) orientation->roll = -180 - orientation->roll;
-  }
+
   /* pitch: Rotation around the lateral axis (the wing span, 'Y axis'). -180<=pitch<=180)     */
   /* pitch is positive and increasing when moving upwards                                     */
   /*                                                                                          */
@@ -126,17 +120,12 @@ bool Adafruit_9DOF::accelGetOrientation(sensors_event_t *event, sensors_vec_t *o
     @brief  Utilize the sensor data from an accelerometer to compensate
             the magnetic sensor measurements when the sensor is tilted
             (the pitch and roll angles are not equal 0�)
-
     @param  axis          The given axis (SENSOR_AXIS_X/Y/Z) that is
                           parallel to the gravity of the Earth
-
     @param  mag_event     The raw magnetometer data to adjust for tilt
-
     @param  accel_event   The accelerometer event data to use to determine
                           the tilt when compensating the mag_event values
-
     @code
-
     // Perform tilt compensation with matching accelerometer data
     sensors_event_t accel_event;
     error = lsm303accelGetSensorEvent(&accel_event);
@@ -144,7 +133,6 @@ bool Adafruit_9DOF::accelGetOrientation(sensors_event_t *event, sensors_vec_t *o
     {
       magTiltCompensation(SENSOR_AXIS_Z, &mag_event, &accel_event);
     }
-
     @endcode
 */
 /**************************************************************************/
@@ -217,21 +205,14 @@ bool Adafruit_9DOF::magTiltCompensation(sensors_axis_t axis, sensors_event_t *ma
 /*!
     @brief  Populates the .heading fields in the sensors_vec_t
             struct with the right angular data (0-359�)
-
             Heading increases when measuring clockwise
-
     @param  axis          The given axis (SENSOR_AXIS_X/Y/Z)
-
     @param  event         The raw magnetometer sensor data to use when
                           calculating out heading
-
     @param  orientation   The sensors_vec_t object where we will
                           assign an 'orientation.heading' value
-
     @code
-
     magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation);
-
     @endcode
 */
 /**************************************************************************/
@@ -268,7 +249,11 @@ bool Adafruit_9DOF::magGetOrientation(sensors_axis_t axis, sensors_event_t *even
       return false;
   }
 
-
+  /* Normalize to 0-359� */
+  if (orientation->heading < 0)
+  {
+    orientation->heading = 360 + orientation->heading;
+  }
 
   return true;
 }
@@ -277,22 +262,16 @@ bool Adafruit_9DOF::magGetOrientation(sensors_axis_t axis, sensors_event_t *even
 /*!
     @brief  Populates the .roll/.pitch/.heading fields in the sensors_vec_t
             struct with the right angular data (in degree).
-
             The starting position is set by placing the object flat and
             pointing northwards (Z-axis pointing upward and X-axis pointing
             northwards).
-
             The orientation of the object can be modeled as resulting from
             3 consecutive rotations in turn: heading (Z-axis), pitch (Y-axis),
             and roll (X-axis) applied to the starting position.
-
-
     @param  accel_event   The sensors_event_t variable containing the
                           data from the accelerometer
-
     @param  mag_event     The sensors_event_t variable containing the
                           data from the magnetometer
-
     @param  orientation   The sensors_vec_t object that will have it's
                           .roll, .pitch and .heading fields populated
 */
@@ -305,8 +284,7 @@ bool Adafruit_9DOF::fusionGetOrientation(sensors_event_t *accel_event, sensors_e
   if ( orientation  == NULL) return false;
 
   float const PI_F = 3.14159265F;
-  float orig_roll;
-  float orig_pitch;
+
   /* roll: Rotation around the X-axis. -180 <= roll <= 180                                          */
   /* a positive roll angle is defined to be a clockwise rotation about the positive X-axis          */
   /*                                                                                                */
@@ -315,12 +293,8 @@ bool Adafruit_9DOF::fusionGetOrientation(sensors_event_t *accel_event, sensors_e
   /*                    z                                                                           */
   /*                                                                                                */
   /* where:  y, z are returned value from accelerometer sensor                                      */
-  orig_roll = (float)atan2(accel_event->acceleration.y, accel_event->acceleration.z);
-  orientation->roll = (float)atan(accel_event->acceleration.y/ sqrt(accel_event->acceleration.x*accel_event->acceleration.x +\
-                                                                     accel_event->acceleration.z*accel_event->acceleration.z));
-  // if(accel_event->acceleration.z < 0){
-  //   orientation->roll = -orientation->roll;
-  // }
+  orientation->roll = (float)atan2(accel_event->acceleration.y, accel_event->acceleration.z);
+
   /* pitch: Rotation around the Y-axis. -180 <= roll <= 180                                         */
   /* a positive pitch angle is defined to be a clockwise rotation about the positive Y-axis         */
   /*                                                                                                */
@@ -330,17 +304,11 @@ bool Adafruit_9DOF::fusionGetOrientation(sensors_event_t *accel_event, sensors_e
   /*                                                                                                */
   /* where:  x, y, z are returned value from accelerometer sensor                                   */
   if (accel_event->acceleration.y * sin(orientation->roll) + accel_event->acceleration.z * cos(orientation->roll) == 0)
-    orig_pitch = accel_event->acceleration.x > 0 ? (PI_F / 2) : (-PI_F / 2);
+    orientation->pitch = accel_event->acceleration.x > 0 ? (PI_F) : (-PI_F);
   else
-    orig_pitch = (float)atan(-accel_event->acceleration.x/ (accel_event->acceleration.y * sin(orientation->roll) + \
+    orientation->pitch = (float)atan2(-accel_event->acceleration.x, (accel_event->acceleration.y * sin(orientation->roll) + \
                                                                      accel_event->acceleration.z * cos(orientation->roll)));
-  // orientation->pitch = (float)atan2(-accel_event->acceleration.x, sqrt(accel_event->acceleration.y * accel_event->acceleration.y + \
-                                                                     // accel_event->acceleration.z * accel_event->acceleration.z));
-  orientation->pitch = (float)atan2(-accel_event->acceleration.x, accel_event->acceleration.z);
-  // if(accel_event->acceleration.z < 0){
-  //   if(orientation->pitch > 0) orientation->pitch = PI_F - orientation->pitch;
-  //   if(orientation->pitch < 0) orientation->pitch = -PI_F - orientation->pitch;
-  // }
+
   /* heading: Rotation around the Z-axis. -180 <= roll <= 180                                       */
   /* a positive heading angle is defined to be a clockwise rotation about the positive Z-axis       */
   /*                                                                                                */
@@ -349,7 +317,7 @@ bool Adafruit_9DOF::fusionGetOrientation(sensors_event_t *accel_event, sensors_e
   /*                    x * cos(pitch) + y * sin(pitch) * sin(roll) + z * sin(pitch) * cos(roll))   */
   /*                                                                                                */
   /* where:  x, y, z are returned value from magnetometer sensor                                    */
-  orientation->heading = (float) atan2(- mag_event->magnetic.z * sin(orientation->roll) - mag_event->magnetic.y * cos(orientation->roll), \
+  orientation->heading = (float)atan2(mag_event->magnetic.z * sin(orientation->roll) - mag_event->magnetic.y * cos(orientation->roll), \
                                       mag_event->magnetic.x * cos(orientation->pitch) + \
                                       mag_event->magnetic.y * sin(orientation->pitch) * sin(orientation->roll) + \
                                       mag_event->magnetic.z * sin(orientation->pitch) * cos(orientation->roll));
@@ -359,10 +327,6 @@ bool Adafruit_9DOF::fusionGetOrientation(sensors_event_t *accel_event, sensors_e
   orientation->roll = orientation->roll * 180 / PI_F;
   orientation->pitch = orientation->pitch * 180 / PI_F;
   orientation->heading = orientation->heading * 180 / PI_F;
-  /* Normalize to 0-359� */
-  if (orientation->heading < 0)
-  {
-    orientation->heading = 360 + orientation->heading;
-  }
+
   return true;
 }
