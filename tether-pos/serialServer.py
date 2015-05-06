65e1c7a44af69088d9360151c2e680335a25c93a
Arthur Lee <arthur@arthurlee.me>
2015-05-06 00:01:22 -0400

    Added serial port server

---
+++ tether-pos/serialServer.py
@@ -0,0 +1,32 @@
+import serial
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 8081)
+print ('starting up on %s port %s' % server_address)
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        ser = serial.Serial('/dev/tty.usbserial-FTHFFDCT', 115200)
+        ser.flushInput()
+        ser.flushOutput()
+
+        while True:
+            line = ser.readline()
+            connection.sendall(line)
+
+    finally:
+        # Clean up the connection
+        ser.close()
+        connection.close()
